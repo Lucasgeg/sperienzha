8d67c3e0feb631345148b8eba2f9ab7a0c547673
Lucasgeg <99218923+Lucasgeg@users.noreply.github.com>
2024-01-03 18:54:47 +0100

    :art: add clerk auth and webhook

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <UserButton afterSignOutUrl="/" />;
+  return <SignOutButton>Déconnexion</SignOutButton>;
 }
